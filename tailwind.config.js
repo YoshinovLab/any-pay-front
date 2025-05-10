231d51105215da03d662667281e0b76f7ebd8f8f
canaria-computer <121685177+canaria-computer@users.noreply.github.com>
2025-05-10 15:59:34 +0900

    ➕ Tailwind CSSを追加

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+// tailwind.config.js
+import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
+export const plugins = [
+  iconsPlugin({
+    collections: getIconCollections(["ic", "noto", "meteocons"]),
+  }),
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -46,5 +47,5 @@ export default defineConfig({
       suppressWarnings: true,
       type: 'module',
     },
-  })],
+  }), tailwindcss()],
 })
