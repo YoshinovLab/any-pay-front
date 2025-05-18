768818c8f3e948c3cec782716a1c7f4f9b9c95b4
canaria-computer <121685177+canaria-computer@users.noreply.github.com>
2025-05-18 13:06:39 +0900

    ✨ Appコンポーネントにfavicon.svgを表示するimgタグを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import PWABadge from "./PWABadge.tsx"
+import icon from "/favicon.svg?url"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <img src={icon} alt="icon" width={100} />
       <button onClick={() => setCount(count + 1)}>{count}</button>
       <PWABadge />
     </>
