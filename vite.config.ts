ca9b5d59019db2cf69145544bf73bcb783864c17
MizunoShota <mizunoshota.13.7.16@gmail.com>
2025-07-02 06:53:21 +0900

    ✨ APIプロキシのパス書き換え処理を追加

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => {
           target: apiBaseUrl,
           changeOrigin: true,
           secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
