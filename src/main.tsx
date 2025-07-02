a6d31b8f66c0a8f1b0121434ff28946136a1b1a6
MizunoShota <mizunoshota.13.7.16@gmail.com>
2025-07-02 15:23:11 +0900

    ✨ ブラウザルーターのbasenameを削除しました

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
 );
+
